ec8533fcf222a62787078ac232829a87c1583d85
awaluwalakhiru <awalprasetyo4@gmail.com>
2019-09-14 18:27:44 +0700

    merubah jawaban 456

--- soal5.php
+++ soal5.php
@@ -1,14 +1,14 @@
 <?php
-$x=10;
-for ($i=1; $i <=$x ; $i++) {
-for ($j=1; $j <= $i; $j++) { 
-    if ($i==$j && $j==$i) {
-        if($i==$j){
-            echo"*";
-            $j++;
+$x = 10;
+for ($i = 1; $i <= $x; $i++) {
+    for ($j = 1; $j <= $i; $j++) {
+        if ($i == $j && $j == $i) {
+            if ($i == $j) {
+                echo "*";
+                $i++;
+            }
+            echo "";
         }
-        echo"";
+        echo "<br/>";
     }
-    echo"<br/>";
-}
 }
